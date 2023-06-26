cfba0b77d92b130f0c57cc66ba90f02a8339fa10
Parul Jain <pjain@LABA.PSWAS.LOCAL>
2023-06-26 15:37:36 -0700

    Added proxy and fixed few bugs

--- server.cjs
+++ server.cjs
@@ -1,17 +1,24 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
 app.use(express.json());
+app.use(cors());
 
 require("dotenv").config();
 
 // Import the veggie model from model folder
-const Veggie = require("./models/veggie");
+const Veggie = require("./models/veggie.cjs");
 
 // Connect to database as soon as the server starts
-require("./config/database.js")
+require("./config/database.cjs");
 
 // Import all the callback functions created in controller for the CRUD operation
-const {  getVeggies, createVeggie, VeggieByName} = require("./controllers/veggies.js")
+const {
+  getVeggies,
+  createVeggie,
+  VeggieByName,
+} = require("./controllers/veggies.cjs");
 
 //Get All the veggies
 app.get("/veggies", getVeggies);
@@ -20,9 +27,8 @@ app.get("/veggies", getVeggies);
 app.post("/create_veggie", createVeggie);
 
 // Get veggie by its name
-app.get("/veggie/:veggieName", VeggieByName)
+app.get("/veggie/:veggieName", VeggieByName);
 
-
-app.listen(4004, ()=>{
-    console.log("Listening on port 4004")
-})
+app.listen(4004, () => {
+  console.log("Listening on port 4004");
+});
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 import axios from "axios";
-import './App.css'
+import "./App.css";
 
 function App() {
   const [formData, setFormData] = useState({
@@ -9,29 +9,32 @@ function App() {
     origin: "",
     price: 0,
     quantity: 1,
-    createdAt: Date.now() ,
+    // createdAt: Date.now(),
   });
 
-  const [veggieArray, setVeggieArray] = useState([])
+  const [veggieArray, setVeggieArray] = useState([]);
 
-  useEffect (() => {
-    axios("/veggies").then((response) =>{
-      console.log(response);
-      setVeggieArray(response.data)
-    })
+  useEffect(() => {
+    axios("/veggies").then((response) => {
+      console.log(response.data);
+      setVeggieArray(response.data);
+    });
   }, []);
 
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData)
-    if (formData.price>0){
+    console.log(formData);
+    if (formData.price > 0) {
       axios({
         method: "POST",
-        url: "/veggies",
-        data: formData
-      })
+        url: "/create_veggie",
+        data: formData,
+      }).then((response) => {
+        console.log(response.data);
+      });
+      setFormData({ name: "", color: "", origin: "", price: 0, quantity: 1 });
     } else {
-      console.log("Price should be greater than 0")
+      console.log("Price should be greater than 0");
     }
   };
 
@@ -42,47 +45,74 @@ function App() {
       [name]: value,
     }));
   };
-  
-  let veggieJSX = veggieArray.map((veggie)=>{
-    return <div key={veggie._id} > {veggie.name} </div>
+
+  let veggieJSX = veggieArray.map((veggie) => {
+    return <div key={veggie._id}> {veggie.name} </div>;
   });
 
   return (
     <>
-    <form id = "veggie_creation_form" onSubmit = {handleSubmit}>
-      <div>
-        <label htmlFor='name'>Veggie Name</label>
-        <input type = "text" name = "name" value = {formData.name} onChange= {handleChange}/>
-      </div>
-      <div>
-        <label htmlFor='color'>Veggie color</label>
-        <input type = "text" name = "color" value = {formData.color} onChange= {handleChange}/>
-      </div>
-      <div>
-        <label htmlFor = "origin"> Veggie Origin </label>
-        <input type = "text" name = "origin" value = {formData.origin} onChange={handleChange} />
-      </div>
-      <div>
-        <label htmlFor = "price"> Veggie Price</label>
-        <input type='number' name = "price" value={formData.price} onChange={handleChange}/>
-      </div>
-      <div>
-        <label htmlFor='quantity'> Veggie quantity</label>
-        <input type='number' name = "quantity" value={formData.quantity} onChange={handleChange} />
-      </div>
-      <div>
-        <label htmlFor='date'> Date </label>
-        <input type='date' name = "Date" value = {formData.Date} onChange={handleChange} />
-      </div>
-      <button>Submit</button>
-    </form>
+      <form id="veggie_creation_form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Veggie Name</label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="color">Veggie color</label>
+          <input
+            type="text"
+            name="color"
+            value={formData.color}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="origin"> Veggie Origin </label>
+          <input
+            type="text"
+            name="origin"
+            value={formData.origin}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="price"> Veggie Price</label>
+          <input
+            type="number"
+            name="price"
+            value={formData.price}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="quantity"> Veggie quantity</label>
+          <input
+            type="number"
+            name="quantity"
+            value={formData.quantity}
+            onChange={handleChange}
+          />
+        </div>
+        {/* <div>
+          <label htmlFor="date"> Date </label>
+          <input
+            type="date"
+            name="Date"
+            value={formData.Date}
+            onChange={handleChange}
+          />
+        </div> */}
+        <button>Submit</button>
+      </form>
 
-    <section>
-      {veggieJSX}
-    </section>
-      
+      <section>{veggieJSX}</section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
